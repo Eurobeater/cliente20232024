c857fa1a42e8c66d47d7131c1fbe5d0ea4d962b0
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2023-10-20 14:13:11 +0200

    Add files via upload

--- Javascript/ej11-elegir-empresa/scripts/ej11.js
+++ Javascript/ej11-elegir-empresa/scripts/ej11.js
@@ -1,9 +1,18 @@
-let empresas = ["Apple","Google","IBM","Microsoft","Nvidia","Intel","Embargos a lo bestia"]
-
 const studentName = document.querySelector("#studentName")
 const CHOICE1 = document.querySelector("#choice1")
 const CHOICE2 = document.querySelector("#choice2")
 const insertButton = document.querySelector("#insertButton")
+
+let empresas = ["Apple","Google","IBM","Microsoft","Nvidia","Intel","Embargos a lo bestia"]
+
+let preferencias = localStorage.getItem("preferencias")
+
+if (preferencias != null) {         //si es valido. tambien vale if (preferencias)
+    preferencias = JSON.parse(preferencias)     //pasarlo a string
+    imprimirPreferencias()              //llamar a la funcion para que se muestren las preferencias guardadas
+}else {
+    preferencias = []
+}
 
 let cleanStudentName = true
 
@@ -25,7 +34,6 @@ function keyPressed() {
     else cleanStudentName = true
     checkButtonDisabled()
 }
-
 
 CHOICE1.addEventListener("change", fillChoice2)     //evento chage para los select
 
@@ -60,4 +68,66 @@ function checkButtonDisabled() {
     if (cleanStudentName == false && CHOICE1.value != 0 && CHOICE2.value != 0)           //if (los 3 campos estan OK)
         insertButton.disabled = false
     else insertButton.disabled = true
+}
+
+insertButton.addEventListener("click",insertarNuevasPreferencias)
+function insertarNuevasPreferencias() {
+    let nombreAlumno = studentName.value.trim()
+    //recuperar el texto del OPTION seleccionado en choice1
+    let nombreEmpresa1 = choice1.options[choice1.selectedIndex].textContent
+    //recuperar el texto del OPTION seleccionado en choice2
+    let nombreEmpresa2 = choice2.options[choice2.selectedIndex].textContent
+    //insertar en el array preferencias un nuevo objeto con los 3 datos introducidos por usuario
+    preferencias.push({
+        alumno: nombreAlumno,
+        empresa1: nombreEmpresa1,
+        empresa2: nombreEmpresa2
+    })
+
+    localStorage.setItem("preferencias", JSON.stringify(preferencias))
+
+    //mostrar en la consola para depurar programa
+    console.table(preferencias)
+    //por último, una vez insertada la nueva info en el array, hay que mostrarlo
+    // en el HTML, en la tabla vacía que hay en la parte de abajo de la web
+    imprimirPreferencias()
+}
+
+function imprimirPreferencias() {
+    studentsChoices.innerHTML = ""
+    preferencias.forEach( pref, indice => {
+        let newTR = studentsChoices.insertRow()
+        let newTD1 = newTR.insertCell()
+        let newTD2 = newTR.insertCell()
+        let newTD3 = newTR.insertCell()
+        let newTD4 = newTR.insertCell()
+        newTD1.textContent = pref.alumno
+        newTD2.textContent = pref.empresa1
+        newTD3.textContent = pref.empresa2
+        let newButton = document.createElement("button")
+        newButton.classList.add("btn","btn-danger")
+        newButton.textContent = "Delete"
+        newTD4.append(newButton)
+        //falta hacer que el botón escuche el evento CLICK
+        newButton.addEventListener("click", function () {
+            preferencias.splice(indice, 1)               //borrar un elemento del array
+            //imprimirPreferencias() //redibuja todo, prodria ser ineficiente
+            newTR.remove()
+            localStorage.setItem("preferencias",JSON.stringify(preferencias))
+        })
+//falta también insertar otros botones para otras acciones
+    let newButtonDown = document.createElement("button")
+    newButtonDown.classList.add("btn", "btn-info")
+    newButtonDown.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"/></svg>'
+        //clic en el boton para bajar (bajar la fila en tabla HTML)
+        newButtonDown.addEventListener("click", function () {
+            //bajar en el array = aumentar su indice
+            if (indice == preferencias.length - 1 ) return
+            let temp = pref
+            preferencias[indice] = preferencias[indice+1]
+            //modificar TABLE
+        })
+
+        
+    })//fin del foreach
 }
