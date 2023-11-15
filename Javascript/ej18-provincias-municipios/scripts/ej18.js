889c571591d6b3aceb6163df2e3d7376d63e8ae2
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2023-11-15 14:30:59 +0100

    Add files via upload

---
+++ Javascript/ej18-provincias-municipios/scripts/ej18.js
@@ -0,0 +1,24 @@
+const selProvincias = document.querySelector("#provincias")
+const selMunicipios = document.querySelector("#municipios")
+
+fetch("server/cargaProvinciasXML.php")
+.then( respuestaHTTP => respuestaHTTP.text())                                                       //se usa text si es un XML (está en texto plano al principio). json para los json
+.then (xmlTextoPlano => {
+    //1. definir un objeto parseador
+    let parser = new DOMParser()                                                    
+    //2. de ese objeto parseador, invocamos el método "parseFromString()
+    let xmlFinal = parser.parseFromString(xmlTextoPlano, "text/xml")   
+    //3.                        
+    let provincias = xmlFinal.querySelectorAll("provincia")                                        //seleccionar todas las provincias que han sido pasadas, para poder recorrerlas
+    provincias.forEach(p => {                                                                       //por cada provincia (p), crear un option
+        //el XML llega en texto plano, hay que parsearlo para convertirlo en un árbol navegable
+        let newOption = document.createElement("option")
+        //el textContent del OPTION quiero que sea el nombre de la provincia,
+        //que es el hijo NOMBRE en el XML
+        newOption.text = p.querySelector("nombre").textContent                                      //acceder al textContent de la etiqueta XML seleccionada
+        //el value del OPTION quiero que sea el código de la provincia,
+        //que es el hijo CÓDIGO en el XML
+        newOption.value = p.querySelector("codigo").textContent
+        selProvincias.append(newOption)
+    })                                           
+})
