46a6c94c79a96c90e17298add40cbd08434d7abd
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2023-11-21 13:31:49 +0100

    Delete Javascript directory

--- Javascript/ej13-billar/scripts/bola.js
+++
@@ -1,9 +0,0 @@
-class Bola {
-    constructor(refDOMbola,x,y,vx,vy) {
-        this.bola = refDOMbola
-        this.posX = x
-        this.posY = y
-        this.velX = vx
-        this.velY = vy
-    }
-}
