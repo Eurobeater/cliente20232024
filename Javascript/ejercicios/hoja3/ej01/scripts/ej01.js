6356369c43ab49b816483f6f39efe1d1a1d71f85
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2023-11-21 13:28:25 +0100

    Delete Javascript/ejercicios/hoja3 directory

--- Javascript/ejercicios/hoja3/ej01/scripts/ej01.js
+++
@@ -1,3 +0,0 @@
-let nombres = ["Antonio", "Rafael", "Marcos", "Carlos", "Javier", "Sofía", "Paula", "Marta", "Belén", "Carlota"]
-let apellidos = ["Rodriguez", "Gallego", "Fernández", "Sabater", "Meseguer", "Zapata", "Bautista", "Morales", "Álvarez", "Castro"]
-let ciudades = ["Murcia", "Albacete", "Valencia", "Jaén", "Córdoba", "Segovia", "Barcelona", "Madrid", "A coruña", "Bilbao"]
