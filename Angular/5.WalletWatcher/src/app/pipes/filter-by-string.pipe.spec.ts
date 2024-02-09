73dc388ffc13e89136f16a4e06641f9dd98fa855
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2024-02-09 13:50:39 +0100

    Delete Angular/5.WalletWatcher/src directory

--- Angular/5.WalletWatcher/src/app/pipes/filter-by-string.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByStringPipe } from './filter-by-string.pipe';
-
-describe('FilterByStringPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByStringPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
