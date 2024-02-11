9d44316827edab2b6473bd6d8b89d84747267c57
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2024-02-12 00:05:19 +0100

    Add files via upload

---
+++ Angular/pruebasExamenes/2.MyWeather/src/app/pipes/patron.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filtroPatron',
+  standalone: true
+})
+export class PatronPipe implements PipeTransform {
+
+  transform(value: any[], patron: string): any[] {
+    if (value && value.length) {
+      return value.filter( a => a.name.toLowerCase().includes( patron.toLowerCase() ) )
+    }else {
+      return []
+    }
+  }
+
+
+}
