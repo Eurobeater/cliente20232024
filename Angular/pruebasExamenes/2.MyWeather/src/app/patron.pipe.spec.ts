91503cbf556a83583c82d5763437da27cfb8a5ae
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2024-02-14 14:16:28 +0100

    Delete Angular/pruebasExamenes/2.MyWeather/src directory

--- Angular/pruebasExamenes/2.MyWeather/src/app/patron.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PatronPipe } from './patron.pipe';
-
-describe('PatronPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PatronPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
